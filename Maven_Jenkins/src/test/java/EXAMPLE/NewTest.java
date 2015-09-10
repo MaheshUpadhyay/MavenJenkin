319bb2db600dad8b22b4e9059b60f907e0eb53ba
Mahesh <Mahesh@192.168.0.6>
2015-09-10 23:07:04 +0530

    initial commit

---
+++ Maven_Jenkins/src/test/java/EXAMPLE/NewTest.java
@@ -0,0 +1,35 @@
+package EXAMPLE;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	private WebDriver driver;       
+    @Test              
+    public void testEasy() {    
+        driver.get("http://www.guru99.com/selenium-tutorial.html");  
+        String title = driver.getTitle();                
+        Assert.assertTrue(title.contains("Free Selenium Tutorials"));       
+    }
+    @Test
+    public void testEasy1() {    
+        driver.get("http://www.google.co.in");  
+        String title = driver.getTitle();                
+        Assert.assertTrue(title.contains("Free Selenium Tutorials"));       
+    }
+    
+    @BeforeTest
+    public void beforeTest() {  
+        driver = new FirefoxDriver();  
+    }       
+    @AfterTest
+    public void afterTest() {
+        driver.quit(); 
+        System.out.println("completed testing");
+    }   
+
+}
